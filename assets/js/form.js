12311badb90c5dc2ef01a5d4e2b97592f5ec41cf
sandratraniaina <sandratrarafa@gmail.com>
2024-05-07 21:41:42 +0300

    refactor: remove loop in form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,11 +1,3 @@
 let buttons = document.querySelectorAll(".menu_btn");
 let forms = document.querySelectorAll(".form-container form");
 
-let isActive = false;
-
-buttons.forEach((button, index) => {
-    button.addEventListener("click", () => {
-        button.classList.toggle("disabled");
-        forms[index].classList.toggle("disabled");
-    });
-});
