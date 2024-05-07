7bf4370e89ca2603e8689a5baa5ef6c14b969c59
sandratraniaina <sandratrarafa@gmail.com>
2024-05-07 22:05:06 +0300

    feat: create resetForm function

--- assets/js/form.js
+++ assets/js/form.js
@@ -4,3 +4,8 @@ let forms = document.querySelectorAll("form");
 let findBtn = buttons[0];
 let addBtn = buttons[1];
 let linkBtn = buttons[2];
+
+const resetForm = (index) => {
+    let form = forms[index];
+    form.reset();
+}
