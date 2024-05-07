35960143e0cfd09c42390010f7c89f1d6ece2fcd
sandratraniaina <sandratrarafa@gmail.com>
2024-05-07 21:50:36 +0300

    feat: Create newServer function

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,29 @@ let container = document.getElementById("cy");
 
 let servers = [];
 
+const newServer = () => {
+    let x = document.getElementById("x").value;
+    let ipAdress = document.getElementById("ip_adress").value;
+    let y = document.getElementById("y").value;
+    let webSites = document.getElementById("webSites").value.split(";");
+    if (x == null || y == null || webSites == null || ipAdress) {
+        alert("Input all data");
+        return;
+    }
+    let newServer = {
+        group: "nodes",
+        data: { id: `${ipAdress}` },
+        position: {
+            x: x,
+            y: y
+        },
+        ipAdress: ipAdress,
+        sites: webSites
+    };
+    cy.add(newServer);
+    servers.push(newServer);
+}
+
 cy.on('tap', function (e) {
     var evtTarget = e.target;
 
